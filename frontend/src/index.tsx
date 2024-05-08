612d5692ab07e398a4a3861766c6fba1aaf130f1
Padla Vili <pvilmos2003@gmail.com>
2024-05-08 17:33:07 +0200

    Implement the Company form

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='main_container w-full h-[100vh] flex justify-center items-center'>
+      <App />
+    </div>
   </React.StrictMode>
 );
--- frontend/src/types/Company.ts
+++ frontend/src/types/Company.ts
@@ -2,7 +2,7 @@ import {z} from 'zod';
 
 console.log('Hello ');
 
-const companySchema = z.object({
+export const companySchema = z.object({
     name: z.string(),
     email: z.string().email('This is not a valid email'),
     numberOfEmployees: z.number().min(1, 'You must render at least 1 employee').max(100, 'You can only render 10 employees at once'),
