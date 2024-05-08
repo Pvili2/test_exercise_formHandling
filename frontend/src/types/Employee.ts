e8c42b569eef36ed18ce5a76ebb7f765213cee02
Padla Vili <pvilmos2003@gmail.com>
2024-05-08 13:48:49 +0200

    Implement a file extension check function
    Implemented a function that checks if the cv property in the employeeSchema is in pdf file format or not

--- frontend/src/types/Employee.ts
+++ frontend/src/types/Employee.ts
@@ -2,12 +2,16 @@ import {z} from 'zod'
 
 const allowedJobs = z.enum(["accountant", "software developer", " software tester", " manager"])
 
+const isPdfFile = (file: File):boolean =>{
+    return file.type ==="pdf";
+}
+
 const employeeSchema = z.object({
     name: z.string(),
     email: z.string().email('This is not a valid email address'),
     job:  allowedJobs,
     age: z.number().min(18).max(120),
-    cv: z.instanceof(File)
+    cv: z.instanceof(File).refine((file) => isPdfFile(file), 'Only .pdf formats are supported.')
 })
 
 export type Employee = z.infer<typeof employeeSchema>;
